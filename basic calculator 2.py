7bd4323b56d4ddc5cc6a2529cd28a4a7881a9a0f
Vineet <vineetkamble35@gmail.com>
2025-08-01 17:43:18 +0530

    Update basic calculator 2.py
    I made changes in the line of codes, where previous code used to take inputs from the users and used to perform only one time and there was no exception handling too. Now, I have added exception handling as well as we can perform various operation as much time as we want.

--- basic calculator 2.py
+++ basic calculator 2.py
@@ -1,24 +1,33 @@
-a = int(input("Enter the first number : "))
-b = int(input('Enter the second number : '))
-c = input("Enter the arithmetic operation you want to perform : ")
-match c :
-    case '+':
-        print("Addition of",a,"and",b,"is",a+b)
-    case '-':
-        print("Substraction of",a,"and",b,"is",a-b)
-    case '*':
-        print("Multiplication of",a,"and",b,"is",a*b)
-    case '/':
-        if b == 0:
-            print("Denominator cannot be zero")
-        else:
-            print("Division of",a,"and",b,"is",a/b)
-    case '**':
-        print("Exponential of",a,"and",b,"is",a**b)
-    case '//':
-        print("Floor division of",a,"and",b,"is",a//b)
-    case '%':
-        print("Modulo of",a,"and",b,"is",a%b)
-    case _:
-        print("Enter a valid arithmetic operation.")
-    
+while True:
+    try:
+        a = int(input('Enter the first number : '))
+        b = int(input('Enter the second number : '))
+        c = input(f'What operation do you wanna perform with {a} and {b}: ')
+        match c :
+            case '+':
+                print("Addition of",a,"and",b,"is",a+b)
+            case '-':
+                print("Substraction of",a,"and",b,"is",a-b)
+            case '*':
+                print("Multiplication of",a,"and",b,"is",a*b)
+            case '/':
+                if b == 0:
+                    print("Denominator cannot be zero")
+                else:
+                    print("Division of",a,"and",b,"is",a/b)
+            case '**':
+                print("Exponential of",a,"and",b,"is",a**b)
+            case '//':
+                print("Floor division of",a,"and",b,"is",a//b)
+            case '%':
+                print("Modulo of",a,"and",b,"is",a%b)
+            case _:
+                print('Enter the valid arithmetic operator.')
+                continue
+        d = input('Do you want to continue? : ')
+        if d != 'yes':
+            print('Hope we will meet again, have a nice day :)')
+            break
+    except:
+        print('Enter the valid numbers.')
+        continue
