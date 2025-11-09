41c6e9b9540c6048349a481b02000262151a9d70
Vineet <vineetkamble35@gmail.com>
2025-11-09 19:34:55 +0530

    Rename variable 'a' to 'number' for clarity

--- readymade_table_generator.py
+++ readymade_table_generator.py
@@ -1,4 +1,4 @@
-a = int(input("Which number's table you want? : "))
+number = int(input("Which number's table you want? : "))
 b = int(input('Till...? : '))
 for i in range(1,b+1):
     print(a,"x",i,"=",a*i)
